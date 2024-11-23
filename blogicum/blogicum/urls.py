ffdaf6bb205bbc251bc66df21ea16086de907035
Niknet19 <nikitos.feo12@gmail.com>
2024-11-23 19:44:26 +0700

    django1 done

---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls', namespace='blog')),
+    path('pages/', include('pages.urls', namespace='pages')),
+]
